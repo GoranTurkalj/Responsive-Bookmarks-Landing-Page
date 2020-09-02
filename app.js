51acb5f8753fbcca6247977607581b786b44138c
GoranTurkalj <turkalj.goran@yahoo.com>
2020-09-02 21:37:23 +0200

    started mobile nav menu

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
-const dList = document.getElementById("d-list");
 
+const body = document.querySelector("body");
+const mainNav = document.getElementById("main-nav");
+const logoImg = document.getElementById("logo"); 
+const navLinksList = document.getElementById("main-nav-list"); 
+const links = navLinksList.querySelectorAll("a"); //actual links in main navigation menu
+const navSocialLinksList = document.getElementById("main-nav-social"); 
+
+
+
+
+//Selects description list for FAQ section
+const dList = document.getElementById("d-list");
+ 
+
+
+//Refers to the hamburger menu control button for mobile view
+const mobileMenuBtn = document.getElementById("mobile-menu-btn");
+const menuBtnImg = document.getElementById("menu-btn-img"); 
+
+//Variable on which behavior of elements depends
+let menuOpened= false; 
+
+
+
+//Controls description list 
 dList.addEventListener("click", (event) => {
   if (event.target.tagName === "BUTTON") {
     console.log(event.target);
@@ -22,4 +46,40 @@ dList.addEventListener("click", (event) => {
         .classList.remove("description-list__arrow--expanded");
     }
   }
+});
+
+
+//Displays or hides mobile navigation
+mobileMenuBtn.addEventListener("click", (event) => {
+  
+    menuOpened = !menuOpened; 
+    
+    if(menuOpened) {
+       
+      //if menuOpened is true  - display the mobile nav
+      mainNav.classList.add("main-nav--mobile-open"); 
+      menuBtnImg.src = "./images/icon-close.svg"; 
+      navLinksList.classList.remove("hidden"); 
+      
+      for(const link of links){
+        link.classList.add("nav__link--mobile"); 
+      }
+
+      navSocialLinksList.classList.remove("hidden"); 
+      body.style.overflow = "hidden"; 
+
+    }
+    else if(!menuOpened) {
+      mainNav.classList.remove("main-nav--mobile-open"); 
+      menuBtnImg.src = "./images/icon-hamburger.svg"; 
+      navLinksList.classList.add("hidden");
+      for (const link of links) {
+        link.classList.remove("nav__link--mobile");
+      }
+      navSocialLinksList.classList.add("hidden"); 
+      body.style.overflow = "auto"; 
+    }
+   
+
+  
 });
